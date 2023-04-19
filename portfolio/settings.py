aa0bf9f00d06a180dc184e5d6b93a58ee7e06c3b
damu <93925813+damu4524l@users.noreply.github.com>
2023-04-19 16:31:36 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,6 +34,8 @@ ALLOWED_HOSTS = ['.vercel.app','.row.sh']
 INSTALLED_APPS = [
     'feedback.apps.FeedbackConfig',
     'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
+    'job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +130,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+
